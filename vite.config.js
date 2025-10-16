ed3bbfe73dd444e44c96fb0808575da6db3c909c
sitaram <srdk7975@gmail.com>
2025-10-16 23:47:55 +0530

    fix2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
-  base:'misteval2',
+  base:'./',
+
   plugins: [react(),tailwindcss()],
 
 })
